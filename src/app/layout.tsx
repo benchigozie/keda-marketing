293d3c41113483638acca061bc027b52cc1e14c3
Ben Chigozie <115552805+benchigozie@users.noreply.github.com.>
2026-01-06 00:39:17 +0100

    chore: update price section wiht the coreect information.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: "Keda Marketing || Africa's No 1 Real Estate Marketing Agency",
   description: "Keda Marketing offers Facebook advertising, website design, video editing, digital marketing, content management, data analysis, and AI automated workflows.",
   icons: {
-  icon: "/images/keda-favicon.webp",
+  icon: "/images/keda-mini.png",
   },
 };
 
--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -10,34 +10,55 @@ function Pricing() {
                 </div>
                 <h2 className="font-bold text-3xl md:text-5xl text-my-white text-center">Packages</h2>
                 <p className="text-center text-lg md:text-xl font-semibold max-w-4xl mx-auto">Our prices are designed to accomodate your budget</p>
-                <div className="grid grid-cols-1 md:grid-cols-3 mt-8 gap-6">
-                    <div className="bg-my-blacker border border-my-black rounded-xl px-6 md:px-10 py-14 my-3 flex flex-col gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-3 mt-8 md:gap-x-6">
+                    <div className="bg-my-blacker border border-my-black rounded-xl px-6 md:px-10 py-14 my-3 flex flex-col gap-6">
+                        <h3 className="text-2xl text-my-lime font-bold">Lite</h3>
+                        <p className="text-my-white text-4xl font-bold">$500</p>
+                        <ul className="list-disc list-inside flex flex-col gap-2 md:text-lg  marker:text-my-lime">
+                            <li>8 Shorts</li>
+                            <li>8 Short Scripts</li>
+                            <li>Content Calender</li>
+                        </ul>
+                        <button className="mt-auto bg-my-lime text-my-black font-bold py-2 px-4 rounded hover:bg-my-lime-dark transition">Choose Plan</button>
+                    </div>
+                    <div className="bg-my-blacker border border-my-black rounded-xl px-6 md:px-10 py-14 my-3 flex flex-col gap-6">
                         <h3 className="text-2xl text-my-lime font-bold">Basic</h3>
-                        <p className="text-my-white text-4xl font-bold">$299</p>
+                        <p className="text-my-white text-4xl font-bold">$750</p>
                         <ul className="list-disc list-inside flex flex-col gap-2 md:text-lg  marker:text-my-lime">
-                            <li>Feature One</li>
-                            <li>Feature Two</li>
-                            <li>Feature Three</li>
+                            <li>8 Short Videos</li>
+                            <li>2 Long Form Videos</li>
+                            <li>8 Short Scripts</li>
+                            <li>2 Long Scripts</li>
+                            <li>Content Calender</li>
                         </ul>
                         <button className="mt-auto bg-my-lime text-my-black font-bold py-2 px-4 rounded hover:bg-my-lime-dark transition">Choose Plan</button>
                     </div>
-                    <div className="bg-my-blacker border border-my-black rounded-xl px-6 md:px-10 py-14 my-3 flex flex-col gap-4">
-                        <h3 className="text-2xl text-my-lime font-bold">Standard</h3>
-                        <p className="text-my-white text-4xl font-bold">$299</p>
+                    <div className="bg-my-blacker border border-my-black rounded-xl px-6 md:px-10 py-14 my-3 flex flex-col gap-6">
+                        <h3 className="text-2xl text-my-lime font-bold">Standard Package</h3>
+                        <p className="text-my-white text-4xl font-bold">$1000</p>
                         <ul className="list-disc list-inside flex flex-col gap-2 md:text-lg  marker:text-my-lime">
-                            <li>Feature One</li>
-                            <li>Feature Two</li>
-                            <li>Feature Three</li>
+                            <li>12 Shorts</li>
+                            <li>2 Long Form Videos</li>
+                            <li>12 Short Scripts</li>
+                            <li>2 Long Scripts</li>
+                            <li>Content Calender</li>
                         </ul>
                         <button className="mt-auto bg-my-lime text-my-black font-bold py-2 px-4 rounded hover:bg-my-lime-dark transition">Choose Plan</button>
                     </div>
-                    <div className="bg-my-blacker border border-my-black rounded-xl px-6 md:px-10 py-14 my-3 flex flex-col gap-4">
+                    <div className="bg-my-blacker border border-my-black rounded-xl px-6 md:px-10 py-14 my-3 flex flex-col gap-6 md:w-max">
                         <h3 className="text-2xl text-my-lime font-bold">Premium</h3>
-                        <p className="text-my-white text-4xl font-bold">$299</p>
+                        <p className="text-my-white text-4xl font-bold">$1500</p>
                         <ul className="list-disc list-inside flex flex-col gap-2 md:text-lg  marker:text-my-lime">
-                            <li>Feature One</li>
-                            <li>Feature Two</li>
-                            <li>Feature Three</li>
+                            <li>12 Short Videos</li>
+                            <li>4 Long Form Videos</li>
+                            <li>12 Short Scripts</li>
+                            <li>4 Long Scripts</li>
+                            <li>4 Thumbnails</li>
+                            <li>12 Portrait Thumbnails.</li>
+                            <li>Content calendar.</li>
+                            <li>Social Media Management/ YouTube Channel Management.</li>
+                            <li>Custom Content Management System.</li>
+                            <li>Content Repurposing.</li>
                         </ul>
                         <button className="mt-auto bg-my-lime text-my-black font-bold py-2 px-4 rounded hover:bg-my-lime-dark transition">Choose Plan</button>
                     </div>
