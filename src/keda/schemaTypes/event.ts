629c613d9f468a12730d537ad3495bc3aa8d7529
Ben Chigozie <115552805+benchigozie@users.noreply.github.com.>
2026-01-06 03:26:45 +0100

    feat: add event schema to sanity studio.

---
+++ src/keda/schemaTypes/event.ts
@@ -0,0 +1,62 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "event",
+  title: "Events",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Event Name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+
+    defineField({
+      name: "image",
+      title: "Event Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+
+    defineField({
+      name: "date",
+      title: "Event Date",
+      type: "date",
+      validation: (Rule) => Rule.required(),
+    }),
+
+    defineField({
+      name: "time",
+      title: "Event Time",
+      type: "string",
+      description: "e.g. 6:30 PM",
+      validation: (Rule) => Rule.required(),
+    }),
+
+    defineField({
+        name: "host",
+        title: "Event Host",
+        type: "reference",
+        to: [{ type: "author" }],
+        validation: (Rule) => Rule.required(),
+      }),
+
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "blockContent",
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      subtitle: "date",
+      media: "image",
+    },
+  },
+});
--- src/lib/sanityImage.ts
+++ src/lib/sanityImage.ts
@@ -1,7 +1,7 @@
-import imageUrlBuilder from '@sanity/image-url'
 import { sanityClient } from './sanityClient'
+import { createImageUrlBuilder } from "@sanity/image-url";
 
-const builder = imageUrlBuilder(sanityClient)
+const builder = createImageUrlBuilder(sanityClient);
 
 export function urlFor(source: any) {
   return builder.image(source)
