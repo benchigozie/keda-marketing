d4ee621c47b4e92f2e2f14c51e27353214d56427
Ben Chigozie <115552805+benchigozie@users.noreply.github.com.>
2026-01-18 22:56:56 +0100

    fix: correct goggle tag script.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,24 +25,20 @@ export default function RootLayout({
 
   const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
-
   return (
     <html lang="en">
       <head>
-        <Script
+      <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
           strategy="afterInteractive"
         />
-
-        <Script id="ga4-init" strategy="afterInteractive">
+        <Script id="google-gtag" strategy="afterInteractive">
           {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${GA_ID}', {
-      page_path: window.location.pathname,
-    });
-  `}
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
         </Script>
       </head>
       <body
