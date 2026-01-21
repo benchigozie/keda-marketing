f051b319103a1a216f3d88c38473117a96a64010
Ben Chigozie <115552805+benchigozie@users.noreply.github.com.>
2026-01-21 23:00:56 +0100

    feat: add metadata to video editing page.

--- src/app/services/video-editing/page.tsx
+++ src/app/services/video-editing/page.tsx
@@ -7,6 +7,13 @@ import VideoShowcase from "@/components/VideoShowcase"
 import WallOfLove from "@/components/WallOfLove"
 import WhatYouGetVideo from "@/components/WhatYouGetVideo"
 import WhyOutsource from "@/components/WhyOutsource"
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Video Editing Services",
+  description: "Enhance your brand with Keda Marketing's video editing services. We specialize in creating engaging videos that convert.",
+};
+
 
 function page() {
   return (
